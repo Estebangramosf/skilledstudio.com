95b59c6027e05563c310785e213be51b5d61f7ae
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-11-19 11:13:36 -0300

    add - installed npm modules, compiling gulp and added js,css,fonts,build bootstrap framework css, configured some routes, and added script in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,44 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
-
+/*
+ elixir(function(mix) {
+ mix.sass('app.scss');
+ });Captura de pantalla de 2016-05-25 14-09-40.png
+ */
+/*
+ elixir(function(mix) {
+ var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+ mix.sass('app.scss')
+ .copy(bootstrapPath + '/fonts/bootstrap/*.{ttf,woff,eof,svg,woff2}', 'public/fonts')
+ .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js');
+ });
+ */
 elixir(function(mix) {
-    mix.sass('app.scss');
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+    var paths = {
+        'bootstrap': '/../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/'
+    }
+
+    mix.sass('app.scss')
+      .copy(bootstrapPath + '/fonts', 'public/fonts')
+      .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js');
+
+    mix.scripts([
+        paths.bootstrap + 'affix.js',
+        paths.bootstrap + 'alert.js',
+        paths.bootstrap + 'button.js',
+        paths.bootstrap + 'carousel.js',
+        paths.bootstrap + 'collapse.js',
+        paths.bootstrap + 'dropdown.js',
+        paths.bootstrap + 'modal.js',
+        paths.bootstrap + 'tooltip.js',
+        paths.bootstrap + 'popover.js',
+        paths.bootstrap + 'scrollspy.js',
+        paths.bootstrap + 'tab.js',
+        paths.bootstrap + 'transition.js',
+    ]);
+
+    mix.version(['public/css/app.css', 'public/js/all.js']);
+
 });
