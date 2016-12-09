336bf5f4f29afca1bcb05ae7be89922ce603c634
Esteban Ramos Fernandez <eramos@celmedia.cl>
2016-12-09 15:47:35 -0300

    update - update field in GalleryImage

--- database/migrations/2016_12_09_181738_create_gallery_images_table.php
+++ database/migrations/2016_12_09_181738_create_gallery_images_table.php
@@ -15,6 +15,8 @@ class CreateGalleryImagesTable extends Migration
         Schema::create('gallery_images', function (Blueprint $table) {
             $table->increments('id');
             $table->string('image',5000);
+            $table->string('title',255);
+            $table->string('body',5000);
             $table->integer('user_id');
             $table->integer('gallery_id');
             $table->timestamps();
