23c6d80c923346d8763a7978b6a70c8aa590d583
Esteban Ramos <esteban.ramos.f@gmail.com>
2017-01-29 13:38:48 -0300

    update - hotfix database

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'skilledstudio'),
+            'database' => env('DB_DATABASE', 'skilledstudio_com'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'skilledstudio2016#'),
             'charset' => 'utf8',
